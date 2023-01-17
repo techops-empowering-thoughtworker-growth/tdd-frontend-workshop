793e6e322d8fee31bf6b5255efa22b45333e1b7d
Yangzihao0126 <511742691@qq.com>
2023-01-17 17:00:12 +0800

    create demo app for refactor

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,19 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { configure, EnzymeAdapter, render } from 'enzyme';
+import axios from 'axios';
 
-test('renders learn react link', () => {
-  render(<App />);
-  expect(screen.getByText('Hello React')).toBeInTheDocument();
+configure({ adapter: new EnzymeAdapter() });
+jest.mock('axios');
+
+describe('App', function () {
+  it('should list correctly', function () {
+    const wrapper = render(<App />);
+    const mockGet = axios.get.mockResolvedValue([
+      { id: 1, name: 'task 01', completed: false },
+      { id: 2, name: 'task 02', completed: false },
+    ]);
+    expect(wrapper).toContain('task 01');
+    expect(wrapper).toContain('task 02');
+    expect(mockGet).toHaveBeenCalled();
+  });
 });
